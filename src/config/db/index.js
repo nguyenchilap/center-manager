ddf1d98579c4a00c48d921b8fd841c3a73938f56
LẬP NGUYỄN CHÍ <18120435@student.hcmus.edu.vn>
2021-11-25 23:39:29 +0700

    add dataabase

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb://localhost:27017/edu_center_management', {
+        await mongoose.connect('mongodb+srv://lap18620:Lapboy20@cluster0.ogmkg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
